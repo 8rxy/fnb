ed212c22fe95ba64794542f5dae5cfba31be0c27
william amminger <wamminge@go.olemiss.edu>
2021-10-31 13:52:40 -0500

    Update Select.js

--- src/pages/Select.js
+++ src/pages/Select.js
@@ -1,25 +1,8 @@
 import React from "react";
 
-import Papa from "papaparse"; 
-import { readString } from "react-papaparse";
 import exampleTable from "./safe_deposit_boxes.csv"
 
-var boxes = ["this is the default value"];
-console.log(typeof boxes)
 
-// makeshift from https://stackoverflow.com/questions/42628426/read-local-csv-with-relative-path-in-reactjs
-const papaConfig = {
-	complete: (results, file) => {
-		console.log('Parsing complete:', results, file);
-		console.log(typeof results);
-		boxes = results
-	},
-	download: true,
-	error: (error, file) => {
-	  console.log('Error while parsing:', error, file);
-	},
-};
-readString(exampleTable, papaConfig);
 
 const Select = () => {
 	return (
@@ -31,11 +14,6 @@ const Select = () => {
 			</p>
 			</header>
 
-			<ul>
-				{boxes.map((boxes) => (
-		  			<li key={boxes}>{boxes}</li>
-				))}
-	  		</ul>
 
 
 
